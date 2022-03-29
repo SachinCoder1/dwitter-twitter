eff69e7265700e0bc3bc2390b5f469c2b9aa5630
SachinCoder1 <sachinhlo232@gmail.com>
2022-03-29 13:52:17 +0530

    writtern first custom smart contract and test for it.

--- contract/contracts/Dwitter.sol
+++ contract/contracts/Dwitter.sol
@@ -4,13 +4,34 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Dwitter {
-    string name;
+   struct User {
+       address wallet;
+       string name;
+       string username;
+       string avatar;
+       string bio;
 
-    function getName() public view returns (string memory) {
-        return name;
-    }
+   }
 
-    function setName(string memory _name) public {
-        name = _name;
-    }
+   mapping (address=>string) public usernames;
+   mapping (string=>User) public users;
+
+   function signup(string memory _username, string memory _name, string memory _bio, string memory _avatar) public {
+       require(bytes(usernames[msg.sender]).length == 0, "User already exists");
+       require(users[_username].wallet == address(0), "Username is taken, please try another one.");
+
+       users[_username] = User({
+           wallet : msg.sender,
+           name: _name,
+           username: _username,
+           bio: _bio,
+           avatar: _avatar
+       });
+       usernames[msg.sender] = _username;
+   }
+
+   function getUser(address _wallet)public view returns(User memory){
+return users[usernames[_wallet]];
+   }
+
 }
---
+++ contract/test/dwitter-test.js
@@ -0,0 +1,35 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Dwitter", function () {
+  it("Should return the name", async function () {
+    const Dwitter = await ethers.getContractFactory("Dwitter");
+    const [user1, user2] = await ethers.getSigners();
+    const dwitter = await Dwitter.deploy();
+    await dwitter.deployed();
+
+
+    await dwitter.signup("hloname", "hlonames", "hlo bio", "hlo avatar");
+    console.log("Signing is in progress for hlo don");
+
+    const user = await dwitter.users("hloname");
+    expect(user.name).to.equal("hlonames");
+    expect(user.bio).to.equal("hlo bio");
+    expect(user.avatar).to.equal("hlo avatar")
+    console.log("user signup is successful, enjoy!!")
+
+    expect(await dwitter.usernames(user1.address)).to.equal("hloname");
+    await expect(dwitter.signup("", "", "", "")).to.be.revertedWith(
+      "User already exists",
+    );
+    console.log("test user already exists error");
+
+
+    await expect(
+      dwitter
+        .connect(user2)
+        .signup("hloname", "hlonames", "hlo bio", "hlo avatar"),
+    ).to.be.revertedWith("Username is taken, please try another one.");
+    console.log("test username is taken error");
+  });
+});
